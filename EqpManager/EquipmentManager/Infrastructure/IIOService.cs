ee3e3c230d7d5754828f7c144e98028ee1777c1d
宇辰 朱 <zhuyc110@qq.com>
2017-12-10 21:46:11 +0800

    Add tooltip to equipment.

--- EqpManager/EquipmentManager/Infrastructure/IIOService.cs
+++ EqpManager/EquipmentManager/Infrastructure/IIOService.cs
@@ -3,5 +3,7 @@
     public interface IIOService
     {
         void ShowDialog<TViewModel>(TViewModel viewModel, DialogSetting dialogSetting = null) where TViewModel : DialogViewModel;
+
+
     }
 }
--- EqpManager/EquipmentManager/ViewModel/Equipment/EquipmentViewModel.cs
+++ EqpManager/EquipmentManager/ViewModel/Equipment/EquipmentViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows.Controls;
 using Prism.Mvvm;
 
 namespace EquipmentManager.ViewModel.Equipment
@@ -6,6 +7,8 @@ namespace EquipmentManager.ViewModel.Equipment
     public class EquipmentViewModel : BindableBase
     {
         public string EquipmentId { get; set; }
+
+        public Control ToolTip => new Button();
 
         public string PackageCode
         {
@@ -16,6 +19,8 @@ namespace EquipmentManager.ViewModel.Equipment
         public int Height { get; set; }
 
         public string StatusChangedTime => _statusChangedTime.ToShortTimeString();
+
+        public string StatusChangedDetailTime => $"{_statusChangedTime.ToShortDateString()} {_statusChangedTime.ToShortTimeString()}";
 
         public EquipmentStatus Status
         {
